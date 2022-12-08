7ff5d54b1c8224452b575ac39b2394ddf620a49f
bosskme <kingsley.onele000@gmail.com>
2022-12-08 04:55:31 -0800

    Hello, Puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(void)/*Main Entry Point*/
+
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);/*Return with the value 0*/
+	return (0);
 }
