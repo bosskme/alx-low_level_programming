9ffee0b7dc9e0e3fba37548ea6f411bfb034fde4
bosskme <kingsley.onele000@gmail.com>
2022-12-19 06:43:23 -0800

    print rev commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+void print_rev(char *);
+
 /**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
 
-void print_rev(char *s) {
-
-int len = strlen(s); // Determine the length of the string
-
-for (int i = len - 1; i >= 0; i--) { // Iterate through string from the end to beginning
-printf("%c", s[i]); // Print character at each iteration
-}
-
-printf("\n"); // Print new line at end of loop
+	str = "Holberton!";
+	print_rev(str);
+	return (0);
 }
