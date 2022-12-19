bf239c7c20e5e28b88a5d902013e71598fc385d1
bosskme <kingsley.onele000@gmail.com>
2022-12-19 02:35:53 -0800

    sorted commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
+ * main - check the code
+ * 
+ * Return: Always 0.
+ */
 void _puts(char *str)
 {
-	for (*str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
-
-	_putchar('\n');
+  for (*str != '\0'; *str++)
+    {
+      _putchar(*str);
+    }
+  _putchar('\n');
 }
