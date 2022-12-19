2c57454556271f2d2d6657571be0d106ac1c30a0
bosskme <kingsley.onele000@gmail.com>
2022-12-19 00:04:05 -0800

    Reset to 98 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- * 
- * Return: Always 0.
- */
-void reset__to_98(int *n);
-int main(void)
+  * reset_to_98 - Updates the value it points to 98
+  * @n: pointer to n
+  *
+  * Return: void
+  */
+void reset_to_98(int *n)
 {
-	int n;
-
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-	printf("%d\n", n);
-	return (0);
+	*n = 98;
 }
