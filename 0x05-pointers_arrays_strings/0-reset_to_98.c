88bd340c8e17a629ddaf72e4900e14d75a2e6592
bosskme <kingsley.onele000@gmail.com>
2022-12-18 23:26:06 -0800

    Reset to 98 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,14 @@
  * 
  * Return: Always 0.
  */
-void reset__to_98(int *n)
+void reset__to_98(int *n);
+int main(void)
 {
-  *n = 98;
+	int n;
+
+	n = 402;
+	printf("%d\n", n);
+	reset_to_98(&n);
+	printf("%d\n", n);
+	return (0);
 }
