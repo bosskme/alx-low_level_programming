ce6448605ba520d00a7681aae19ab851ec7be966
bosskme <kingsley.onele000@gmail.com>
2022-12-21 19:15:07 -0800

    concatenation commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,20 @@
-#include “main.h”
+#include "main.h"
 
 /**
-  * _strcat - Concatenates two strings
-  * @dest: The destination string
-  * @src: The source string
-  *
-  * Return: A pointer to the resulting string dest
-  */
-char *_strcat(char *dest, char *src)
+* strcat - Concatenates the string pointed to by @src, including the x
+* null byte, to the end of the string pointed to by @dest.
+* @dest: A pointer to the string to be concatenated upon.
+* @src: The source string to be appended to @dest.
+*
+* Return: A pointer to the destination string @dest.
+*/
+
+char *_strcat(char *dest, const char *src)
 {
-  int index = 0, dest_len = 0;
-  while (dest[index++])
-    dest_len++;
-  for (index = 0; src[index]; index++) 
-    dest[dest_len++] = src[index];
-  return (dest);
+	int index = 0, dest_len = 0;
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
