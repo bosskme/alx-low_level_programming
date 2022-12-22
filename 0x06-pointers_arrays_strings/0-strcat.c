b9cae048116ab725774895c57ec5662f1009bf08
bosskme <kingsley.onele000@gmail.com>
2022-12-21 19:20:00 -0800

    concatenation commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-* strcat - Concatenates the string pointed to by @src, including the x
-* null byte, to the end of the string pointed to by @dest.
-* @dest: A pointer to the string to be concatenated upon.
-* @src: The source string to be appended to @dest.
+* _strcat - Concatenates two strings
+* @dest: The destination string
+* @src: The source string
 *
-* Return: A pointer to the destination string @dest.
+* Return: A pointer to the resulting string dest
 */
 
 char *_strcat(char *dest, char *src)
