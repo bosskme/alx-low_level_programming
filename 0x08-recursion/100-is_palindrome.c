ce72336464d7e71727a93065885ce404ce7fb8a5
bosskme <kingsley.onele000@gmail.com>
2023-01-04 04:13:06 -0800

    Palindrome commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,62 +1,28 @@
 #include "main.h"
 
 /**
- * is_palindrome -  sss
- * @r: string
- * Return: return something
+ *File: 100-os_palindrome.c
+ *Auth:Kachi
  */
 
-int is_palindrome(char *r)
-{
-
-	int i, size, add;
-
-	i = 0;
-	add = 0;
-
-	size = _strlen_recursion(r);
-
-	add = (size % 2 != 0) ? 2 : 1;
-
-	return (evaluate(r, i, size - 1, add));
-}
+int find_strlen(char *s);
+int check_palindrome(har *s, int len, int index);
+int is_palindrome(char *s);
 
 /**
- * evaluate - compare
- * @i: i
- * @size: size
- * @s: string
- * @add: addition depending if n is odd or even
- * Return: return value
+ * find_strlen - Returns the length of a string.
+ *@s: The string to be measured.
+ *
+ *Return: The length of the string.
  */
-int evaluate(char *s, int i, int size, int add)
+int find_strlen(char *s)
 {
+  int len = 0;
 
-	if (i + add == size  && s[i] == s[size])
-	{
-		return (1);
-	}
-
-	else if (s[i] == s[size])
-	{
-		return (evaluate(s, i + 1, size - 1, add));
-	}
-
-	return (0);
-}
-
-/**
- * _strlen_recursion - legth of a string
- * @s: string
- * Return: return legth
- */
-
-int _strlen_recursion(char *s)
-{
-	/*Base condition*/
-	if (!*s)
-		return (0);
-
-	else
-		return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
+  if(*(s + len))
+    {
+      len++;
+      len += find_strlen(s + len);
+    }
+  return(len);
 }
