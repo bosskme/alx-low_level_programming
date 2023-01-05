48eb26a8a1e036e94cf4d22294a10cc1b51685bd
bosskme <kingsley.onele000@gmail.com>
2023-01-04 20:59:12 -0800

    6 is_prime_number commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,42 +1,21 @@
 #include "main.h"
+
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
 /**
- *_evaluate - evaluate function sqrt
- *@i: interger
- *@n: interger
- *Return: evaluate sqrt
+ *is_divisible - Checks if a number is divisible.
+ *@num: The number is checked.
+ *@div: The divisor.
+ *
+ *Return: If the number is divisible - 0.
+ *       If the number is not divisible - 1.
  */
-
-int _evaluate(int i, int n)
+int is_divisible(int num, int div)
 {
-	/*Evaluate function*/
-	if (n == 0 || n == 1)
-		return (n);
-
-	else if (i * i < n)
-		return (_evaluate(i + 1, n));
-
-	else if (i * i == n) /*Condiction base*/
-		return (i);
-
-	return (-1);
-}
-
-/**
- *_sqrt_recursion - evaluate sqrt
- *@n: interger
- *Return: Sqrt_recursion
-*/
-
-int _sqrt_recursion(int n)
-{
-	int i = 0;
-
-	if (i < 0) /*If n is negative*/
-		return (-1);
-
-	else
-	{
-		return (_evaluate(i, n)); /*Recursive call*/
-	}
-
+  if(num % div == 0)
+    return(0);
+  if(div == num / 2)
+    return(1);
+  return(is_divisible(num, div + 1));
 }
