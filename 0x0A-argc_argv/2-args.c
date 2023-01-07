10ba08d354b824c6804c2ef5931cfb2b72eb788d
bosskme <kingsley.onele000@gmail.com>
2023-01-06 22:32:46 -0800

    2 args commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,26 +1,21 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
-*main - print the name of the program.
-*@argc: Count arguments
-*@argv: Arguments
-*
-*Return: Always 0 (Success)
-*/
-
-int main (int argc, char *argv[])
+  * main - Prints all the args content
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
 {
-  int count = 0;
+	int i;
 
-  if(argc > 0)
-    {
-      while (count < argc)
+	for (i = 0; i < argc; i++)
 	{
-	  printf("%s\n", argv[count]);
-	  return (0);
+		printf("%s\n", argv[i]);
 	}
-      printf("Error\n");
-      return (1);
-    }
+
+	return (0);
 }
