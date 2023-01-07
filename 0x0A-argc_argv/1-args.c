202ebdac0196189c2c6a8afd8f84124bb08674fc
bosskme <kingsley.onele000@gmail.com>
2023-01-06 22:28:46 -0800

    1 args commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,16 @@
+#include "holberton.h"
 #include <stdio.h>
-#include "main.h"
 
 /**
- *main - print the name of the program.
- *@argc: Count arguments.
- *@argv: Arguments.
- *
- *Return: Always 0 (Success)
- */
-
+  * main - Prints the number of args
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Always zero
+  */
 int main(int argc, char *argv[])
 {
-  (void) argv; /*ignore argv*/
-  printf("%i\n", argc - 1);
-
-  return (0);
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
 }
