de560e9527ef926d6eb3adc0b96fe4dd29628152
bosskme <kingsley.onele000@gmail.com>
2023-02-02 03:48:28 -0800

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <stddef.h>
-#include <math.h>
+#include <stdlib.h>
 
 /**
  * binary_to_uint - Convert a string representing a binary number to an
@@ -10,26 +9,27 @@
  * Return: 0 if string contains something other than 1 or 0, or
  * if the string is NULL, return the decimal value on success
  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, len;
+	unsigned int decval;
 
-unsigned int binary_to_unit(const char *b){
-  unsigned int result = 0;
-
-  if (b == NULL) {
-    return 0;
-  }
-
-  size_t length = 0;
-  while (b[length] != '\0') {
-    length++;
-  }
-
-  for (int i = length - 1; i >= 0; i--) {
-    if (b[i] == '0' || b[i] == '1') {
-      result += (b[i] - '0') * pow(2, length - i - 1);
-    } else {
-      return 0;
-    }
-  }
-
-  return result;
+	i = len = decval = 0;
+	if (b == NULL)
+		return (0);
+	while (b[len] != '\0')
+	{
+		if (b[len] == '0' || b[len] == '1')
+			len++;
+		else
+			return (0);
+	}
+	while (i < len)
+	{
+		decval = decval << 1;
+		if (b[i] == '1')
+			decval += 1;
+		i++;
+	}
+	return (decval);
 }
