8fd10570d1624e8b87003692fc78ad412f4d6fa1
bosskme <kingsley.onele000@gmail.com>
2023-03-21 02:02:17 -0700

    creating dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-#compile all the .c files into object files
 gcc -c -Wall _Werror -fpic *.c
-
-#create the dynamic library
 gcc -shared -o liball.so *.o
+export LD_Library_PATH=.:$LD_LIBRARY_PATH
