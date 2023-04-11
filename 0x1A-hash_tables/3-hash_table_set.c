c2338047b2d70379b71d6ac0e7b7ea5b8f61e4c5
bosskme <kingsley.onele000@gmail.com>
2023-04-11 08:06:24 -0700

    function that adds an element to hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,42 +13,45 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
-    hash_node_t *new_node, *tmp;
+unsigned long int index;
+hash_node_t *new_node, *tmp;
 
-    if (!ht || !key || !*key)
-        return (0);
+if (!ht || !key || !*key)
+return (0);
 
-    index = key_index((unsigned char *)key, ht->size);
+index = key_index((unsigned char *)key, ht->size);
 
-    tmp = ht->array[index];
-    while (tmp)
-    {
-        if (!strcmp(tmp->key, key))
-        {
-            free(tmp->value);
-            tmp->value = strdup(value);
-            return (1);
-        }
-        tmp = tmp->next;
-    }
+tmp = ht->array[index];
 
-    new_node = malloc(sizeof(hash_node_t));
-    if (!new_node)
-        return (0);
+while (tmp)
+{
+if (!strcmp(tmp->key, key))
+{
+free(tmp->value);
+tmp->value = strdup(value);
 
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
-    if (!new_node->key || !new_node->value)
-    {
-        free(new_node->key);
-        free(new_node->value);
-        free(new_node);
-        return (0);
-    }
+return (1);
+}
+tmp = tmp->next;
+}
 
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+new_node = malloc(sizeof(hash_node_t));
 
-    return (1);
+if (!new_node)
+
+return (0);
+
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+
+if (!new_node->key || !new_node->value)
+{
+free(new_node->key);
+free(new_node->value);
+free(new_node);
+return (0);
+}
+
+new_node->next = ht->array[index];
+ return (1);
 }
