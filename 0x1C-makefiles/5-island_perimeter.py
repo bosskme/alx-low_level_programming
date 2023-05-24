1446fafb58fff560bebd07e14a08eaf4cb17d9c2
bosskme <kingsley.onele000@gmail.com>
2023-05-24 03:28:10 -0700

    Island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,21 +12,17 @@ Returns:
     int: The permeter of the island.
 
 """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-perimeter = 0
-rows = len(grid)
-cols = len(grid[0])
-
-for i in range(rows):
-    for j in range(cols):
-        if grid[i][j] == 1:
-            perimeter += 4
-
-            if i > 0 and grid[i - 1][j] == 1:
-                perimeter -= 2
-
-            if j > 0 and grid[i][j - 1] == 1:
-                perimeter -= 2
-
-    return perimeter
-            
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
